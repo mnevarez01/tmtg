0dc388c72e067bb6b497017249a60c7afe8804d9
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-07 13:28:02 -0600

    psuedo coding and layout

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//click event for button
+//var userInput = 
+//var wikiHowAPI =
+//var drinksAPI=
+//function for wikihow: DO you want an alcoholic or non-alcoholic drink---button 
+// var alcohol = true
+// var alcohol = false 
+//event.prevetDefault
+//function for drinks--if its true 
