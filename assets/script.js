186c29db13a2a6f71e5d2098cc79d8c7580d98e7
Genet worku <genetmworku@gmail.com>
2020-04-08 12:16:30 -0600

    update-2

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,28 @@
 
 
-
-$("#run-everything-button").on("click", function (event) {
-    event.preventDefault;
-    var userInput = $("#input").val();
-    wikiHow(userInput);
-})
-
-function wikiHow(userInput) {
-    console.log(userInput)
-    $.ajax({
-        async: true,
-        crossDomain: true,
-        url: "https://hargrimm-wikihow-v1.p.rapidapi.com/steps?count=" + userInput,
-        method: "GET",
-        headers: {
-            "x-rapidapi-host": "hargrimm-wikihow-v1.p.rapidapi.com",
-            "x-rapidapi-key": "d8ec69564cmsh8bfdd3439a39718p14736fjsn7795f327adcd"
-        }
-    }).done(function (response) {
-        console.log(response);
-        $("#wikiHow").append(response);
-    });
-};
-
-
-
-
-
-
-
-
-
-
-
-
+	$("#run-everything-button").on("click", function (event) {
+		event.preventDefault;
+		var userInput = $("#input").val();
+		wikiHow(userInput);
+	});
+	
+	function wikiHow(userInput) {
+		console.log(userInput)
+		$.ajax({
+			async: true,
+			crossDomain: true,
+			url: "https://hargrimm-wikihow-v1.p.rapidapi.com/steps?count=" + userInput,
+			method: "GET",
+			headers: {
+				"x-rapidapi-host": "hargrimm-wikihow-v1.p.rapidapi.com",
+				"x-rapidapi-key": "d8ec69564cmsh8bfdd3439a39718p14736fjsn7795f327adcd"
+			}
+		}).done(function (response) {
+			event.preventDefault;
+			console.log(response);
+			$("#wikiHow").append(response);
+		});
+	};
 
 
 
